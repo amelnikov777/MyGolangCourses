6e9e15d4c040762f3148332b408e31ccd7167bc5
Andrei Melnikov <125220410+amelnikov777@users.noreply.github.com>
2023-03-06 22:50:45 +0200

    Update version of 2.3Animals.go course2 week3

--- 2.3Animals.go
+++ 2.3Animals.go
@@ -14,54 +14,59 @@ type animal struct {
 }
 
 func (p animal) eat(name string) {
-	fmt.Println(name, "eats: ", p.food)
+	fmt.Println(name, " eats: ", p.food)
 }
 
 func (p animal) move(name string) {
-	fmt.Println(name, "moves: ", p.loco)
+	fmt.Println(name, " locomotion: ", p.loco)
 }
 
 func (p animal) speak(name string) {
-	fmt.Println(name, "speaks: ", p.sound)
+	fmt.Println(name, " speak: ", p.sound)
 }
 
-// Func userprompt lets to input just command words in one line with space
-func userprompt() (anim, animinfo string) {
+//Func userprompt lets to input just correct command words in one line with space
+func userprompt() (anim string, info string) {
+	var commands = []string{"cow eat", "cow move", "cow speak", "bird eat", "bird move", "bird speak", "snake eat", "snake move", "snake speak"}
+	t := false
 	fmt.Println("Input animal(cow, bird, snake) and function (eat, move, speak)> ")
-	animinfo, _ = bufio.NewReader(os.Stdin).ReadString('\n')
-	animinfo = strings.Trim(animinfo, "\r\n")
-	inputstring := strings.Split(animinfo, " ")
-	anim = inputstring[0]
+	inputline, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	inputline = strings.Trim(inputline, "\r\n")
+	inputline = strings.ReplaceAll(inputline, "  ", " ")
+	for _, i := range commands {
+		if i == inputline {
+			t = true
+			break
+		}
+	}
+	if t == false {
+		fmt.Println("Invalid command. Try again.\n")
+		return
+		userprompt()
+	}
+	inputstring := strings.Split(inputline, " ")
+	anim, info = inputstring[0], inputstring[1]
 	return
 }
 
 func main() {
-	var cow = animal{food: "grass", loco: "walk", sound: "moo"}
-	var bird = animal{food: "worms", loco: "fly", sound: "peep"}
-	var snake = animal{food: "mice", loco: "slither", sound: "hsss"}
-	var anim, animinfo string
-	for {
-		anim, animinfo = userprompt()
+	var animals = map[string]animal{
+		"cow":   animal{"grass", "walk", "moo"},
+		"bird":  animal{"worms", "fly", "peep"},
+		"snake": animal{"mice", "slither", "hsss"},
+	}
 
-		switch animinfo {
-		case "cow eat":
-			cow.eat(anim)
-		case "cow move":
-			cow.move(anim)
-		case "cow speak":
-			cow.speak(anim)
-		case "bir deat":
-			bird.eat(anim)
-		case "bird move":
-			bird.move(anim)
-		case "bird speak":
-			bird.speak(anim)
-		case "snake eat":
-			snake.eat(anim)
-		case "snake move":
-			snake.move(anim)
-		case "snake speak":
-			snake.speak(anim)
+	var anim, info string
+	for {
+		anim, info = userprompt()
+
+		switch info {
+		case "eat":
+			animals[anim].eat(anim)
+		case "move":
+			animals[anim].move(anim)
+		case "speak":
+			animals[anim].speak(anim)
 		}
 	}
 }
