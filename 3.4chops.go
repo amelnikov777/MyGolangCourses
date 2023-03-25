ef8345cf598fa745d586c2d2bf002c3938a911b0
Andrei Melnikov <125220410+amelnikov777@users.noreply.github.com>
2023-03-25 22:27:54 +0300

    Update Course 3 Week 4

--- 3.4chops.go
+++ 3.4chops.go
@@ -1,4 +1,4 @@
-// chops
+// 5 philosophes eat rice with 5 chopsticks
 package main
 
 import (
@@ -22,9 +22,10 @@ func main() {
 	for i := 0; i < 5; i++ {
 		philos[i] = &Philo{CSticks[i], CSticks[(i+1)%5]}
 	}
-	hostCh_r := make(chan int, 2)     //Buffered request channel limiting simultaneous permissions for two goroutines
-	hostCh_a := make(chan int)        //Permission channel
-	phil_rand := rand.Intn(40)/10 + 5 //The first eating philosophe is random
+
+	hostCh_r := make(chan int, 2)          //Buffered request channel limiting simultaneous permissions for two goroutines
+	hostCh_a := make(chan int)             //Permission channel
+	phil_rand := (rand.Intn(40)+10)/10 + 5 //The first eating philosophe is random
 
 	go host(hostCh_r, hostCh_a) //Starting host
 
@@ -32,37 +33,37 @@ func main() {
 		phil_eat := (phil_rand + i) % 5
 		go philos[phil_eat].eat(phil_eat, hostCh_r, hostCh_a)
 	}
-
-	for pause := 0; pause < 1000000000; pause++ {
-	} //Pause for finish all goroutines
+	pause(1000000000) //Pause for finishing all goroutines
 }
 
-//In this metod each philosof asks permission from host and starts eating is steacks ate free
+//In this method each philosophe asks permission from host and starts eating if chopsticks are free
 func (p Philo) eat(phil int, hostCh_r, hostCh_a chan int) {
 	phil_eat := phil + 1
 	for t := 1; t < 4; t++ {
 		p.leftCS.Lock()
 		p.rightCS.Lock()
 		hostCh_r <- phil_eat
-		fmt.Println("Request from philosophe", phil_eat)
+		fmt.Println("Host request from philosop.\t", phil_eat)
 		permission := <-hostCh_a
 		permission++
-		fmt.Println("start eating phil.\t", phil_eat, "time", t)
-		for eating_pause := 0; eating_pause < 1000000; eating_pause++ {
-		} //Waiting for eating process
-		fmt.Println("finish eating phil.\t", phil_eat, "time", t)
+		fmt.Println("\tstarting to eat\t", phil_eat, "time", t)
+		pause(10000000) //Waiting for eating process
+		fmt.Println("\tfinishing eating\t", phil_eat, "time", t)
 		p.rightCS.Unlock()
 		p.leftCS.Unlock()
 	}
 }
 
-//Host function check requests from "eat" method and gives permissions
+//Host function checks requests from "eat" method and gives permissions
 func host(hostCh_r chan int, hostCh_a chan int) { //host
 	for {
 		req := <-hostCh_r
-		for host_pause := 0; host_pause < 100; host_pause++ {
-		}
+		pause(1000)
 		hostCh_a <- req
+	}
+}
 
+func pause(time int) {
+	for pause := 0; pause < time; pause++ {
 	}
 }
